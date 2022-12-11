4a9d6ae2c47cedc86b24c77722528f6a991db1b8
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-12-11 12:35:53 -0500

    middleware-practica

---
+++ milddeware/customHeader.js
@@ -0,0 +1,19 @@
+
+
+const customHeader = (req, res, next) => {
+    try {
+        const apiKey = req.headers.api_key
+        if (apiKey === 'sergio-01') {
+            next()
+        } else {
+            res.status(403)
+            res.send({ error: "API_KEY_NO_ES_CORRECTA" })
+        }
+
+    } catch (e) {
+        res.status(403)
+        res.send({ error: "ALGO_OCURRIO_EN_EL_CUSTOM_HEADER" })
+    }
+}
+
+module.exports = customHeader
