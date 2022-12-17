57312109186325b58d675dc001721ae55265b60c
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-12-17 07:28:26 -0500

    bcrypt

--- models/noSql/users.js
+++ models/noSql/users.js
@@ -1,41 +1,30 @@
-const mongoose = require('mongoose')
-const mongooseDelete = require('mongoose-delete')
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+const UserScheme = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+        },
+        age: {
+            type: Number,
+        },
+        email: {
+            type: String,
+            unique: true,
+        },
+        password: {
+            type: String,
+        },
+        role: {
+            type: ["user", "admin"],
+            default: "user",
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
 
-const UserSchema = new mongoose.Schema
-    (
-        {
-            name: {
-                type: String,
-
-            }
-        },
-        {
-            age: {
-                type: Number
-            }
-        },
-        {
-            email: {
-                type: String,
-                unique: true
-            }
-        },
-        {
-            passwords: {
-                type: String,
-            }
-        },
-        {
-            role: {
-                type: ["user", "admin"],
-                default: "user"
-            }
-        },
-        {
-            timestamps: true,// me va a marcar las fechas de actualizaciuón
-            versionKey: false
-        }
-
-    )
-UserSchema.plugin(mongooseDelete, { overrideMethods: 'all' })//para sobre escribir los metodos que vienen nativos de mongo
-module.exports = mongoose.model("users", UserSchema)
+UserScheme.plugin(mongooseDelete, { overrideMethods: "all" });
+module.exports = mongoose.model("users", UserScheme);
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const router = express.Router();
 const { matchedData } = require('express-validator')
+const { encrypt, compare } = require('../utils/handdlePassword')
+const { usersModel } = require('../models')
+const router = express.Router();
 
 const { validatorRegister, validatorLogin } = require('../validators/auth')
 
-router.post('/register', validatorRegister, (req, res) => {
+router.post('/register', validatorRegister, async (req, res) => {
     req = matchedData(req)
-
-    res.send({ data: req })
+    const password = await encrypt(req.password)
+    const body = { ...req, password }//aqui estamos creando un objeto nuevo(..req) y la sobre escribimos con la propiedad pasword
+    const data = await usersModel.create(body)
+    data.set('password', undefined), {
+        strict: false
+    }//quiero setear la propiedad password con undefined y que no se atan estricto (en model users agregamos select false para esta propiedad)
+    res.send({ data })
 })
 module.exports = router
